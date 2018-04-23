a4c9b9ee04f7d25067a07f01e0eecdccc959e25a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication1/Window1.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpo;
+using System.ComponentModel;
+using DevExpress.Wpf.Grid;
+using System.Windows.Threading;
+using DevExpress.Wpf.Editors.Settings;
+using System.Threading;
+using DevExpress.Data;
+using System.Windows.Markup;
+
+namespace WpfApplication1 {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+
+        ViewModel vm;
+        public Window1() {
+            InitializeComponent();
+            vm = new ViewModel();
+            this.DataContext = vm;
+            gridControl1.DataSource = vm.GetData();
+
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            vm.CurrentRow = gridControl1.GetRow(1);
+
+        }
+
+
+    }
+
+    public class ViewModel : DependencyObject
+    {
+
+        public static DependencyProperty CurrentRowProperty =
+   DependencyProperty.Register("CurrentRow", typeof(object),
+                               typeof(ViewModel));
+        public object CurrentRow
+        {
+            get { return (object)GetValue(CurrentRowProperty); }
+            set { SetValue(CurrentRowProperty, value); }
+        }
+
+        BindingList<ContactItem> coll;
+        public object GetData()
+        {
+            coll = new BindingList<ContactItem>();
+            ContactItem f1 = new ContactItem();
+            f1.Name = "Abcd bla bl1111a  ";
+            f1.Salary = 13d;
+            f1.Date = DateTime.Today;
+            f1.IsFired = false;
+            coll.Add(f1);
+            f1 = new ContactItem();
+            f1.Name = "bla1";
+            f1.Date = DateTime.Today;
+            f1.Salary = 13;
+
+            coll.Add(f1);
+
+            return coll;
+        }
+
+        
+    }
+
+}
---
+++ VB/WpfApplication1/Window1.xaml.vb
@@ -0,0 +1,80 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+Imports DevExpress.Xpo
+Imports System.ComponentModel
+Imports DevExpress.Wpf.Grid
+Imports System.Windows.Threading
+Imports DevExpress.Wpf.Editors.Settings
+Imports System.Threading
+Imports DevExpress.Data
+Imports System.Windows.Markup
+
+Namespace WpfApplication1
+	''' <summary>
+	''' Interaction logic for Window1.xaml
+	''' </summary>
+	Partial Public Class Window1
+		Inherits Window
+
+		Private vm As ViewModel
+		Public Sub New()
+			InitializeComponent()
+			vm = New ViewModel()
+			Me.DataContext = vm
+			gridControl1.DataSource = vm.GetData()
+
+
+		End Sub
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			vm.CurrentRow = gridControl1.GetRow(1)
+
+		End Sub
+
+
+	End Class
+
+	Public Class ViewModel
+		Inherits DependencyObject
+
+		Public Shared CurrentRowProperty As DependencyProperty = DependencyProperty.Register("CurrentRow", GetType(Object), GetType(ViewModel))
+		Public Property CurrentRow() As Object
+			Get
+				Return CObj(GetValue(CurrentRowProperty))
+			End Get
+			Set(ByVal value As Object)
+				SetValue(CurrentRowProperty, value)
+			End Set
+		End Property
+
+		Private coll As BindingList(Of ContactItem)
+		Public Function GetData() As Object
+			coll = New BindingList(Of ContactItem)()
+			Dim f1 As New ContactItem()
+			f1.Name = "Abcd bla bl1111a  "
+			f1.Salary = 13R
+            f1.IsFired = False
+			coll.Add(f1)
+			f1 = New ContactItem()
+			f1.Name = "bla1"
+            f1.Salary = 13
+            coll.Add(f1)
+            Return coll
+		End Function
+
+
+	End Class
+
+End Namespace
